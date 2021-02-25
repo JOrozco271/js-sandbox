1e6cee3e18312adb8481fb7ccd48cf28801ca893
Joshua <jorozco271@west-mec.org>
2021-02-25 09:58:34 -0700

    We finshed our color and working on dark mode command

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let colorSelect = document.getElementById("colorSelect");
 colorSelect.onchange = function () {
     console.log(colorSelect.value);
+    document.getElementById("colorBox").style = `background-color: ${colorSelect.value};`;
+}
+function toggleDarkMode(){
+    alert("Toggle ME")
 }
