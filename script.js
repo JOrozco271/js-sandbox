0defad795113e710dbcc99a1ab8a3f5f0f97839d
Joshua <jorozco271@west-mec.org>
2021-03-03 08:46:11 -0700

    Teleport Button

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function toggleDarkMode(){
     content.classList.toggle("dark-mode")
     //alert("Toggle ME")
 }
-function teleportButton(){
-    console.log("teleport");
+function teleportButton(distance){
     let runaway = document.getElementById("runaway");
+    console.log(runaway.style.top), " - ", runaway.style.left
+    if (runaway.style.top == 0 && runaway.style.left == 0) {
+        runaway.style.top = `${distance}px`;
+    }else if (runaway.style.top == `${distance}px` && runaway.style.left == 0) {
+        runaway.style.left = `${distance}px`;
+    }else if (runaway.style.top == `${distance}px` && runaway.style.left == `${distance}px`) {
+        runaway.style.top = 0;
+    }else if (runaway.style.top == "0px" && runaway.style.left == `${distance}px`) {
+        runaway.style.left = null;
+        runaway.style.top = null;
+    }
     runaway.innerText = "Dont Touch ME!";
 }
